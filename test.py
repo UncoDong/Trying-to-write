0d5c3aa6a679996937354c72aa828da61fdb5686
UncleDong <1758322248@qq.com>
2020-06-12 23:22:28 +0800

    hi

---
+++ test.py
@@ -0,0 +1,9 @@
+def fun(num):
+    for n in num:
+        for m in num:
+            if m == 0:
+                for i in range(10):
+                    print('hi')
+    
+num = [1,2,3,4,5,6,7]
+fun(num)
